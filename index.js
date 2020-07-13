95cab13cef0714334c2f6779eecb83c2770411c7
berellevy <berellevy@gmail.com>
2020-07-13 12:56:48 -0400

    Done.

--- index.js
+++ index.js
@@ -3,11 +3,25 @@
 document.querySelector("button").addEventListener("click", handleClick);
 
 function handleClick(e) {
-  const timeString = document.getElementById('time').value
-  displayMessage(greet(timeString))
+    const timeString = document.getElementById('time').value
+    displayMessage(greet(timeString))
 }
 
 /* End Given Code, don't edit above here...*/
 
+
+
 /* Write your implementation of greet() */
-/* Write your implementation of displayMessage() */
+function greet(timeString) {
+    const hour = parseInt(timeString)
+    if (hour < 12) return "Good Morning"
+    if (hour > 17) return "Good Evening"
+    return "Good Afternoon"
+}
+
+
+/* Write your implementation of displayMessage()*/
+function displayMessage(text) {
+    document.getElementById('greeting').innerText = text
+
+}
