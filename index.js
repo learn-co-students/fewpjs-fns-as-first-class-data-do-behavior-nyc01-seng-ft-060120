c3e9a659e8e110b8e67f5f286cbf4bce9b4f3c07
aaltmark <adamaltmark@gmail.com>
2020-07-13 14:41:17 -0400

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,19 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  const split_time = time.split(':'); //["HH", "MM"]
+  const parse_time = parseInt(split_time[0])
+  if (parse_time < 12) {
+    return "Good Morning"
+  } else if (parse_time > 12 && parse_time < 18) {
+    return "Good Afternoon"
+  } else {
+    return "Good Evening"
+  }
+}
 /* Write your implementation of displayMessage() */
+
+function displayMessage(text) {
+  document.getElementById("greeting").innerText = text;
+}
