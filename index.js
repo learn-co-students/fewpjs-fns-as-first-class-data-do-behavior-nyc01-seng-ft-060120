32e3cb5d50b6ee1c55ad565e127aaafff18f8d9c
alexpac27 <alexpac27@gmail.com>
2020-07-13 19:08:50 -0400

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,22 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(time){
+let newTime = time.split(":")
+  if (newTime[0] > 17){
+    return "Good Evening";
+  }
+  else if (newTime[0] < 12){
+    return "Good Morning";
+  }
+  else if (newTime[0] > 12 && newTime[0] < 17){
+    return "Good Afternoon";
+  }
+}
 /* Write your implementation of displayMessage() */
+
+function displayMessage(string){
+  document.getElementById('greeting').innerText = string;
+    
+}
